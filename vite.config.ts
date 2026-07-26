1507bfb445e35375e17da874b4a58a5c41bd4c89
iririn1998 <iririn199810710@gmail.com>
2026-07-26 21:22:32 +0900

    build: set base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
